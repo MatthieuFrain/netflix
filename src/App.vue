c0a1afe0cff2a04a2767a0f5cde3a5a876a5ccce
agirlnamedmat <74149836+agirlnamedmat@users.noreply.github.com>
2022-01-06 11:38:29 +0100

    modifications du script

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,16 @@
 <template>
 <header>
-  <div>
+  <div class="container">
     <img class="logo" src="./medias/Netflux-logo.png" alt="logo de netflux">
-  </div>
     <div class="nav-bar">
-  <a href="">
-    Acceuil
-  </a>
-
- <a href="">
-    Série
-  </a>
-
- <a href="">
-    Film
-  </a>
-  
-   <a href="">
-    Nouveautés les plus regardées 
-  </a>
-
- <a href="">
-    Ma liste 
-  </a>
+      <ul>
+        <li><a href="#">Accueil</a></li>
+        <li><a href="#">Séries</a></li>
+        <li><a href="#">Films</a></li>
+        <li><a href="#">Nouveautés les plus regardées</a></li>
+        <li><a href="#">Ma liste</a></li>
+      </ul>
+  </div>
   </div>
 </header>
 </template>
@@ -43,9 +31,19 @@ export default {
 body{
   background-color: black;
 }
+
+.container{
+  display: flex;
+  align-content: center;
+  margin-top: 20px;
+}
 .logo{
   width: 120px;
   margin-left: 100px;
-  margin-top: 20px;
+
+}
+.nav-bar,ul{
+  display: flex;
+
 }
 </style>
