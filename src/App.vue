52e70c4272170ed7f6b7ed8ddb4b7e4182f56d25
SoumiyaB <sbezzou213@gmail.com>
2022-01-06 13:31:28 +0100

    test video 6

--- src/App.vue
+++ src/App.vue
@@ -13,9 +13,7 @@
   </div>
   </div>
 </header>
-<iframe width="560" height="315" src="https://www.youtube.com/watch?v=_434vl32oJY"
-frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope;
-picture-in-picture" allowfullscreen></iframe>
+
 </template>
 
 <script>
