0ad063c7914e88dc7fac12f6e6bc31bf51e940aa
Matthieu FRAIN <matthieu.frain@gmail.com>
2022-01-06 13:47:50 +0100

    changements apportés

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,18 @@
 <template>
-
+  <affichageApi title="TOP 100 - Films les plus populaires" type="MostPopularMovies"/>
 </template>
 
 <script>
-
+import affichageApi from './components/affichageApi.vue'
 
 export default {
   name: 'App',
   components: {
-
+    affichageApi
   }
 }
 </script>
 
-<style lang="css">
-#app {
+<style>
 
-}
 </style>
---
+++ src/components/affichageApi.vue
@@ -0,0 +1,37 @@
+<template>
+<h3 class="titreCategorie">{{title}}</h3>
+<div class="contenuCategorie" v-for="result in post" :key="result">
+  <div class="categorie">
+    <b>{{result.rank}}</b>
+    <img :src="result.image">
+    <b>{{result.title}}</b>
+    <b>{{result.imDbRating}} / 10</b>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'affichageApi',
+  data() {
+    return {
+      post : []
+    }
+  },
+  props: [
+    "type",
+    "title" 
+  ],
+  mounted(){
+    const axios = require('axios').default;
+    let url = "https://imdb-api.com/en/API/"+this.type+"/k_orp1gt36";
+    axios
+      .get(url)
+      .then(response => (this.post = response.data.items))
+  }
+}
+</script>
+
+<style lang="css">
+
+</style>
